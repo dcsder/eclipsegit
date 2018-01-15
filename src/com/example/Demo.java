a550a3693c88cd7b1931408e5489288d6905e96e
hungrypan <1602944312@qq.com>
2018-01-15 22:17:45 +0800

    create project base resources

---
+++ src/com/example/Demo.java
@@ -0,0 +1,23 @@
+/**
+* <p>Title: Demo.java</p>
+* <p>Description: </p>
+* <p>Copyright: Copyright (c) 2018</p>
+* @author panyc
+* @date 2018年1月15日
+* @version 1.0
+*/
+package com.example;
+
+/**
+ * Demo
+ * <p>Description:</p>
+ * @author panyc
+ * @class Demo
+ * @date 2018年1月15日
+ * @version 1.0
+ */
+public class Demo {
+	public static void main(String[] args) {
+		System.out.println("hello git");
+	}
+}
